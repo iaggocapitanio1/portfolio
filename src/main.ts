7d43887d0a98585abe5cf76496714024d8426071
iaggocapitanio1 <iaggo.capitanio@gmail.com>
2025-07-01 15:06:43 -0300

    addign favicons

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,29 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
-import { faGithub, faLinkedin, faTwitter, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+  faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faMapMarkerAlt, faPhone } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import { i18n } from './i18n/i18n.config'
 import router from './router'
 const app = createApp(App)
 
-library.add(faEnvelope, faPhone, faMapMarkerAlt, faGithub, faLinkedin, faTwitter, faWhatsapp)
+library.add(
+  faEnvelope,
+  faPhone,
+  faMapMarkerAlt,
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faWhatsapp,
+  faInstagram,
+)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(i18n)
 app.use(createPinia())
